17204e5a45a68c85b730841edeb8ead40d84e0c3
Codewithdark <codewithdark90@gmail.com>
2024-07-05 13:02:28 +0500

    Create ocr.py

---
+++ utils/ocr.py
@@ -0,0 +1,6 @@
+import pytesseract
+
+def image_to_text(image):
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    text = pytesseract.image_to_string(image)
+    return text
