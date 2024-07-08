529c3cfe30bd471404c8ee971cbd7ba11e9750b3
Codewithdark <codewithdark90@gmail.com>
2024-07-08 21:04:42 +0500

    Update ocr.py

--- utils/ocr.py
+++ utils/ocr.py
@@ -1,5 +1,4 @@
 import pytesseract
-
 def image_to_text(image):
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
     text = pytesseract.image_to_string(image)
