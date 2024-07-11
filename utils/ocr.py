64e7c4b1720e75d18f4d4f4a576b7266867de5cc
Codewithdark <codewithdark90@gmail.com>
2024-07-11 17:30:38 +0500

    Update ocr.py

--- utils/ocr.py
+++ utils/ocr.py
@@ -1,6 +1,14 @@
 import pytesseract
+import easyocr
+
+# def image_to_text(image):
+#     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+#     text = pytesseract.image_to_string(image)
+#     return text
 
 def image_to_text(image):
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-    text = pytesseract.image_to_string(image)
-    return text
+    reader = easyocr.Reader(['ch_sim', 'en'])  # this needs to run only once to load the model into memory
+    result = reader.readtext(image, detail=0)
+    return result
+
+
